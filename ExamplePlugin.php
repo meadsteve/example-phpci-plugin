68aae9884dd165b52a5e58589a92343812103ea3
meadsteve <meadsteve@gmail.com>
2013-12-29 16:08:17 +0000

    Initial commit

---
+++ ExamplePlugin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace meadsteve\PhpciPlugins;
+
+use PHPCI\Plugin;
+
+class ExamplePlugin implements Plugin
+{
+    public function execute()
+    {
+        // TODO: Implement execute() method.
+    }
+} 
